4561934634ce74545ecc66df18ab70304db2b81e
Lena <lena_rthr@icloud.com>
2015-05-05 21:32:26 -0500

    added data validation

--- ChunkOffsets.py
+++ ChunkOffsets.py
@@ -1,31 +1,53 @@
 __author__ = 'lena'
+import string
+
+
+def check_iterations(any_iterations):
+    idx = 0
+    found = False
+    while idx < 10 and not found:
+        if any_iterations[0] == string.digits[idx]:
+            found = True
+            return found
+        else:
+            idx += 1
+    return found
 
 
 def next_offset(any_offset):
-    addOn = int("10410", base=16)
+    add_on = int("10410", base=16)
+    new_offset = (any_offset + add_on)
 
-    newOffset = (any_offset + addOn)
-
-    return newOffset
+    return new_offset
 
 
-def print_offset(anyOffset, anyIterations):
+def print_offset(any_offset, any_iterations):
 
     print("OFFSETS\n------")
-    print(hex(anyOffset), "\n------")
+    print(hex(any_offset), "\n------")
 
-    for i in range(anyIterations):
-        anyOffset = next_offset(anyOffset)
-        print(hex(anyOffset))
+    for i in range(any_iterations):
+        any_offset = next_offset(any_offset)
+        print(hex(any_offset))
         print("-------")
 
 
 def main():
 
-    myOffset = int(input("Enter an offset (first is 'c000c')\n"), base=16)
-    myIterations = int(input("How many offsets to generate?\n"))
+    hex_chunk = int("10410", base=16)
+    my_offset = int(input("Enter an offset (first is 'c000c')\n"), base=16)
+    # 54108 is the calculated remainder (base 10) of a valid chunk // 10410
+    while my_offset % hex_chunk != 54108:
+        my_offset = int(input("Enter a valid chunk offset number please\n"), base=16)
 
-    print_offset(myOffset, myIterations)
+    my_iterations = input("How many offsets to generate? 1 - oo\n")
+    while not check_iterations(my_iterations):
+        my_iterations = input("You must enter a number\n")
+        check_iterations(my_iterations)
+
+    my_iterations = int(my_iterations)
+
+    print_offset(my_offset, my_iterations)
     print("this is a test")
 
 
---
+++ testing.py
@@ -0,0 +1,17 @@
+# HEX CHECKS
+#
+# a = int("10410", base=16)
+# print(a)
+#
+# b = int("c000c", base=16)
+# print(b)
+#
+# c = int("0xd041c", base=16)
+#
+# print(b % a)
+# print(hex(c % a))
+
+import string
+
+for i in range(500):
+    print(uni, "\n")
