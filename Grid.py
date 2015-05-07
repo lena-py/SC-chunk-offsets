32a92459ec87e6d03318c93054ee0fd27b562667
Lena <lena_rthr@icloud.com>
2015-05-07 13:45:49 -0500

    worked on grid

--- Grid.py
+++ Grid.py
@@ -13,46 +13,50 @@ c = get_input(input("Enter an offset (first is 'c000c')\n"), string.hexdigits,
 import turtle
 slim = turtle.Turtle()
 wn = turtle.Screen()
-wn.setworldcoordinates(-20, -20, 320, 320)
+wn.setworldcoordinates(-20, -20, 1960, 320)
 slim.speed(0)
+x = 0
 
-a = (c) + 20
-b = hex(a)
-a = b
+a = c + 20
+a = hex(a)
+# a = b
 slim.hideturtle()
 slim.penup()
 slim.goto(0, 320)
-slim.write(b[2:], move=False, align="center", font=("Arial", 16, "bold"))
+slim.write(a[2:], move=False, align="center", font=("Arial", 16, "bold"))
 y = 300
 
 
 for i in range(15):
     a = int(a, base=16) + 256
-    b = hex(a)
-    a = b
+    a = hex(a)
+    # a = b
     slim.goto(0, y)
-    slim.write(b[2:], move=False, align="center", font=("Arial", 16, "bold"))
+    slim.write(a[2:], move=False, align="center", font=("Arial", 16, "bold"))
     y -= 20
 
-x = 50
-y = 320
+# x = 50
+# y = 320
 
 
-def column(a, b, x, y):
+def column(a, x, y):
     for idx in range(16):
         a = int(a, base=16) + 256
-        b = hex(a)
-        a = b
+        a = hex(a)
+        # a = b
         slim.goto(x, y)
-        slim.write(b[2:], move=False, align="center", font=("Arial", 16, "bold"))
+        slim.write(a[2:], move=False, align="center", font=("Arial", 16, "bold"))
         y -= 20
-        # return a,b,x,y
-for test in range(5):
-    column(a, b, x, y)
+    return a
+
+for test in range(15):
+    x += 150
+    y = 320
+    a = column(a, x, y)
+    #column(a, x, y)
     #a = int(a, base=16) + 256
 
-    x += 50
-    y = 320
+
 
 # column(a, b, x, y)
 
